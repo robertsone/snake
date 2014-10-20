af99df0679c1fb69a3f198e9836c1dbeb6ad9a6e
robertsone <rufrrufr@yahoo.com>
2014-10-20 08:43:56 -0400

    class

--- WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -19,12 +19,16 @@ namespace WindowsGame1
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Random rand = new Random();
-        List<Vector2> snake = new List<Vector2>();
+        
         Texture2D squareTexture;
-        int updatetimer = 0;
-        int snakesize = 16;
-        string der = "UP";
 
+        Snake snake;
+        Vector2 food = new Vector2(10,10);
+        int snakesize = 16;
+
+        bool spawnfood = true;
+
+        
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -43,16 +47,12 @@ namespace WindowsGame1
         
         protected override void LoadContent()
         {
-            
 
+            
             spriteBatch = new SpriteBatch(GraphicsDevice);
             squareTexture = Content.Load<Texture2D>(@"SQUARE");
 
-            snake.Add(new Vector2(1, 1));
-            snake.Add(new Vector2(1, 2));
-            snake.Add(new Vector2(2, 2));
-            snake.Add(new Vector2(2, 3));
-            snake.Add(new Vector2(2, 4));
+            snake = new Snake(squareTexture, new Vector2(20, 20));
 
         }
 
@@ -65,56 +65,26 @@ namespace WindowsGame1
         
         protected override void Update(GameTime gameTime)
         {
-            
+
+            /*
+            if (snake[0] == food)
+            {
+                spawnfood = true;
+                toadd = 2;
+            }
+            */
+
+            if (spawnfood == true)
+            {
+                food = new Vector2(rand.Next(1,49), rand.Next(1,29));
+                spawnfood = false;
+            }
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-            KeyboardState kb = Keyboard.GetState();
-            if (kb.IsKeyDown(Keys.Down) || der == "DOWN")
-                der = "DOWN";
-            else if (kb.IsKeyDown(Keys.Left) || der == "LEFT")
-                der = "LEFT";
-            else if (kb.IsKeyDown(Keys.Up) || der == "UP")
-                der = "UP";
-            else if (kb.IsKeyDown(Keys.Right) || der == "RIGHT")
-                der = "RIGHT";
 
-            updatetimer += 1;
-            if (updatetimer >= 10)
-            {
-                updatetimer = 0;
-                for (int i = 0; i < snake.Count; i++)
-                {
-                    int newi = snake.Count - i-1;
-                    if (newi != 0)
-                        snake[newi] = snake[newi - 1];
-                    else
-                    {
+            snake.Update(gameTime);
 
-                        if (der == "DOWN")
-                        {
-                            der = "DOWN";
-                            snake[0] = new Vector2(snake[0].X, snake[0].Y + 1);
-                        }
-                        else if (der == "LEFT")
-                        {
-                            der = "LEFT";
-                            snake[0] = new Vector2(snake[0].X - 1, snake[0].Y);
-                        }
-                        else if (der == "UP")
-                        {
-                            der = "UP";
-                            snake[0] = new Vector2(snake[0].X, snake[0].Y - 1);
-                        }
-                        else if (der == "RIGHT")
-                        {
-                            der = "RIGHT";
-                            snake[0] = new Vector2(snake[0].X + 1, snake[0].Y);
-                        }
-                    }
-
-                }
-            }
             
 
             base.Update(gameTime);
@@ -125,10 +95,10 @@ namespace WindowsGame1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-            for (int i = 0; i<snake.Count; i++)
-            {
-                spriteBatch.Draw(squareTexture, new Rectangle((int)snake[i].X * snakesize, (int)snake[i].Y * snakesize, snakesize, snakesize), new Rectangle(0, 0, snakesize, snakesize), Color.Red);
-            }
+
+            snake.Draw(spriteBatch);
+
+            spriteBatch.Draw(squareTexture, new Rectangle((int)food.X * snakesize, (int)food.Y * snakesize, snakesize, snakesize), new Rectangle(0, 0, snakesize, snakesize), Color.Green);
             spriteBatch.End();
             base.Draw(gameTime);
         }
---
+++ WindowsGame1/WindowsGame1/WindowsGame1/Snake.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    class Snake
+    {
+        string der = "LEFT";
+        public List<Vector2> snake = new List<Vector2>();
+        private Texture2D snakeTexture;
+        int updatetimer = 0;
+        bool lose = false;
+        int toadd = 0;
+        int snakesize = 16;
+
+        public Snake(Texture2D texture, Vector2 location)
+        {
+            this.snakeTexture = texture;
+
+            snake.Add(location);
+            snake.Add(location + new Vector2(0, 1));
+            snake.Add(location + new Vector2(0, 2));
+            snake.Add(location + new Vector2(0, 3));
+            snake.Add(location + new Vector2(0, 4));
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState kb = Keyboard.GetState();
+            if (kb.IsKeyDown(Keys.Down) && der != "UP")
+                der = "DOWN";
+            if (kb.IsKeyDown(Keys.Left) && der != "RIGHT")
+                der = "LEFT";
+            if (kb.IsKeyDown(Keys.Up) && der != "DOWN")
+                der = "UP";
+            if (kb.IsKeyDown(Keys.Right) && der != "LEFT")
+                der = "RIGHT";
+
+            updatetimer += 1;
+            if (updatetimer >= 5 && lose != true)
+            {
+                updatetimer = 0;
+                for (int i = 0; i < snake.Count; i++)
+                {
+                    int newi = snake.Count - i - 1;
+                    if (newi != 0)
+                        if (toadd >= 1 && newi == snake.Count - 1)
+                        {
+                            toadd -= 1;
+                            snake.Add(snake[newi]);
+                            snake[newi] = snake[newi - 1];
+
+                        }
+                        else
+                        {
+                            snake[newi] = snake[newi - 1];
+                        }
+                    else
+                    {
+
+                        if (der == "DOWN")
+                        {
+                            snake[0] = new Vector2(snake[0].X, snake[0].Y + 1);
+                        }
+                        if (der == "LEFT")
+                        {
+                            snake[0] = new Vector2(snake[0].X - 1, snake[0].Y);
+                        }
+                        if (der == "UP")
+                        {
+                            snake[0] = new Vector2(snake[0].X, snake[0].Y - 1);
+                        }
+                        if (der == "RIGHT")
+                        {
+                            snake[0] = new Vector2(snake[0].X + 1, snake[0].Y);
+                        }
+                    }
+
+                }
+            }
+            for (int i = 0; i < snake.Count; i++)
+            {
+                if (snake[0] == snake[i] && i != 0)
+                {
+                    lose = true;
+                    int thing = i;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < snake.Count; i++)
+            {
+                spriteBatch.Draw(snakeTexture, new Rectangle((int)snake[i].X * snakesize, (int)snake[i].Y * snakesize, snakesize, snakesize), new Rectangle(0, 0, snakesize, snakesize), Color.Red);
+            }
+        }
+    }
+}
